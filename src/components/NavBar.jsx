ca97830e4e60c26eb24e8c7ceaa5c7ff20be2257
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-06 17:43:17 -0400

    basic file structure restructred, mainpage, footer, navbar initialized

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <a className="nav-link active" href="/">
+        Sam Sherrill's Portfolio
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
